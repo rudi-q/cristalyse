ba8b6722308e1fbba23ab7e98842d10c562a5c4e
Rudi K. <rudi@cristalyse.com>
2025-09-03 01:19:10 +0800

    optimized imports

--- test/bubble_chart_integration_test.dart
+++ test/bubble_chart_integration_test.dart
@@ -1,7 +1,8 @@
 import 'dart:math' as math;
+
+import 'package:cristalyse/cristalyse.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:cristalyse/cristalyse.dart';
 
 void main() {
   group('Bubble Chart Integration - FINAL VERIFICATION', () {
